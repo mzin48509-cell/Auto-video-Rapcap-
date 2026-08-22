d7d996c3560c8fa56d0ac49b91afe110064f526c
mzin48509-cell <mzin48509@gmail.com>
2026-08-23 00:35:39 +0630

    Update server.js

--- server.js
+++ server.js
@@ -262,6 +262,7 @@ app.post("/api/render", upload.single("video"), async (req,res) => {
   }
 });
 
-app.get("(.*)", (_req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+app.get(/.*/, (_req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
 
 app.listen(PORT,()=>console.log(`AI Video Translator: http://localhost:${PORT}`));
